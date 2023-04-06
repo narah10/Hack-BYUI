e84568878328e432b30a750818d063dfdbadcecc
narah10 <kwa18001@byui.edu>
2023-04-05 22:45:02 -0600

    worked on some css and footer

---
+++ src/components/footer.js
@@ -0,0 +1,39 @@
+import React from "react";
+import footer from "./footer.module.css";
+import byui from "./assets/logo.svg";
+
+function Footer() {
+  return (
+    <div class={footer.background}>
+      <div class={footer.backgroundContainer}>
+        <div class={footer.logoSection}>
+          <img src={byui} />
+          <h1>Hack BYU-I</h1>
+        </div>
+        <div>
+          <h3>Contact</h3>
+        </div>
+        <div>
+          <h3>Links</h3>
+          <div class={footer.link}>
+            <a href="#">Home</a>
+            <a href="#">Agenda</a>
+            <a href="#">FAQ</a>
+            <a href="#">Sponsor</a>
+            <a href="#">Volunteer</a>
+            <a href="#">Location</a>
+          </div>
+        </div>
+        <div>
+          <h3>Our Team</h3>
+          <p>Want to meet the amazing team that made all this happen?</p>
+        </div>
+      </div>
+      <p id={footer.copyright}>
+        Copyright &copy; 2023 BYUI. All rights reserved{" "}
+      </p>
+    </div>
+  );
+}
+
+export default Footer;
--- src/components/homepage/card.js
+++ src/components/homepage/card.js
@@ -4,7 +4,7 @@ import card from "./card.module.css";
 function Card({ imageurl, title, body }) {
   return (
     <div className={card.container}>
-      <img src={imageurl} alt="" />
+      <img src={imageurl} alt="" class={card.cardStyle} />
       <h3>{title}</h3>
       <p>{body}</p>
     </div>
