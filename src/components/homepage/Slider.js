5a77fb0e83c12e650dd5c75b19a6e277d866dce9
gilber <gilber1002@gmail.com>
2023-04-06 00:42:55 -0600

    slider update

--- src/components/homepage/Slider.js
+++ src/components/homepage/Slider.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import slider from './Slider.module.css';
+import leftbutton from '../assets/leftbutton.svg';
+import rightbutton from '../assets/rightbutton.svg';
 
 
 function Slider() {
     const slides = [1,2,3,4,5,6,7,8];
+    const slideLeft = ()=>{
+        var slider = document.getElementById("slider")
+        slider.scrollLeft = slider.scrollLeft + 300;
+    }
+    const slideright = ()=>{
+        var slider = document.getElementById("slider")
+        slider.scrollLeft = slider.scrollLeft - 300;
+    }
   return (
     <div className={slider.container}>
         <div className={slider.buttonleft}>
-            left
+            <img src={leftbutton} alt='' onClick={slideLeft}/>
         </div>
-        <div className={slider.slider}>
+        <div className={slider.slider} id='slider'>
             {
                 slides.map((slide,index)=>{
                     return(
@@ -19,8 +29,8 @@ function Slider() {
                 })
             }
         </div>
-        <div className={slider.buttonright}>
-            right
+        <div className={slider.buttonright} onClick={slideright}> 
+            <img src={rightbutton} alt=''/>
         </div>       
     </div>
   )
