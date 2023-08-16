7ec1e7d8432e11786b98756fb2c58cd7cc27405b
Danny Hernandez <dxherny@gmail.com>
2023-08-16 10:35:04 -0600

    sponsors

--- src/components/Sponsor/sponsor.js
+++ src/components/Sponsor/sponsor.js
@@ -1,23 +1,26 @@
 import React from "react";
-import sponsor from "./sponsor.module.css"
-import github from "../assets/github-logo.svg"
-import standOut from "../assets/stand-out-stickers-logo.png"
-import frozen from "../assets/frozenYoghurt.png"
-import verizon from "../assets/verizon.png"
-
+import sponsor from "./sponsor.module.css";
+import github from "../assets/github-logo.svg";
+import standOut from "../assets/stand-out-stickers-logo.png";
+import frozen from "../assets/frozenYoghurt.png";
+import verizon from "../assets/verizon.png";
+import byui_cit from "../assets/byui-cit-dep.png";
+import byui_cse from "../assets/byui-cse-dep.png";
 
 function Sponsor() {
-    return (
-        <div className={sponsor.container}>
-            <h2>Our Sponsors</h2>
-            <div className={sponsor.items}>
-                <img src={github}/>
-                <img src={standOut}/>
-                <img src={frozen}/>
-                <img src={verizon}/>
-            </div>
-        </div>
-    )
+  return (
+    <div className={sponsor.container}>
+      <h2>Our Sponsors</h2>
+      <div className={sponsor.items}>
+        <img src={github} />
+        <img src={standOut} />
+        <img src={frozen} />
+        <img src={verizon} />
+        <img src={byui_cit} />
+        <img src={byui_cse} />
+      </div>
+    </div>
+  );
 }
 
 export default Sponsor;
